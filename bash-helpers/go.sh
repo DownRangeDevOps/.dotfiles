fbf5c03059fb731f221823f5e7b1b522ae934971
Ryan Fisher <ryan@downrangedevops.com>
2023-08-28 22:23:28 -0700

    Lazy load goenv

--- bash-helpers/go.sh
+++ bash-helpers/go.sh
@@ -4,33 +4,38 @@ log debug "$(printf_callout ["${BASH_SOURCE[0]}"])"
 
 # lazy init goenv
 function goenv_alias() {
-	local go_cmds
-	mapfile -t go_cmds <<-EOF
-		        go
-		        goenv
-	EOF
+    local go_cmds
+    mapfile -t go_cmds <<-EOF
+go
+goenv
+EOF
 
-	for cmd in "${go_cmds[@]}"; do
-		cmd=$(printf "%s" "${cmd}" | trim)
+    for cmd in "${go_cmds[@]}"; do
+        cmd=$(printf "%s" "${cmd}" | trim)
 
-		if [[ $1 == "create" ]]; then
-			# shellcheck disable=SC2139
-			alias "${cmd}"=goenv_lazy_init
-		elif [[ $1 == "remove" ]]; then
-			unalias "${cmd}" 2>/dev/null
-		fi
-	done
+        if [[ $1 == "create" ]]; then
+            # shellcheck disable=SC2139
+            alias "${cmd}=goenv_lazy_init"
+        elif [[ $1 == "remove" ]]; then
+            unalias "${cmd}" 2>/dev/null
+        fi
+    done
 }
 
 function goenv_lazy_init() {
+    local last_cmd
+    last_cmd=$(fc -l | tail -1 | cut -d ' ' -f 2-)
+
     log debug "$(printf_callout Initalizing goenv...)"
 	unset -f goenv_lazy_init
 
-	printf_callout "%s\n" "goenv has not been initialized, initializing now..."
-	goenv_alias remove
-	eval "$(goenv init -)"
+    printf_warning "goenv has not been initialized, initializing now..." >&2
 
-	printf_warning "%s\n" "goenv initialized, plese re-run the previosu command."
+    goenv_alias remove
+    eval "$(goenv init -)"
+    add_to_path "prepend" "$(goenv prefix)/bin" # Go binaries
+
+    $last_cmd
 }
 
 if [[ -z ${GOENV_ROOT:-} ]]; then
