aa2908458ec30493979db2853506b00c6de22c4a
Ryan Fisher <ryan@downrangedevops.com>
2025-01-23 12:13:27 -0800

    Make antidote plugin paths generic

--- config/.zsh_plugins.zsh
+++ config/.zsh_plugins.zsh
@@ -1,2 +1,2 @@
-fpath+=( /Users/xjxf277/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-jeffreytse-SLASH-zsh-vi-mode )
-source /Users/xjxf277/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-jeffreytse-SLASH-zsh-vi-mode/zsh-vi-mode.plugin.zsh
+fpath+=( $HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-jeffreytse-SLASH-zsh-vi-mode )
+source $HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-jeffreytse-SLASH-zsh-vi-mode/zsh-vi-mode.plugin.zsh
